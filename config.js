70495bc43816f844c844a3f51bcab5809b85f890
YourIceMyIce <162663921+YourIceMyIce@users.noreply.github.com>
2024-03-08 13:30:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ms Linh';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Chúc Pé';
+const contentCard = 'haha haha haha haha Haha haha haha haha Haha haha haha haha Haha haha haha 8/3 haha haha haha haha Haha haha haha haha Haha haha haha haha Haha haha haha haha💗💗💗';
+
+const giftImage = 'linh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
